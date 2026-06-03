9e8d16d90abc3a05a98fecd3ece19f8a6997fbee
elCubil <rodriguez.arias.miguel@gmail.com>
2026-06-03 15:26:44 -0500

    commit inicial inclute .gitignore modificado

---
+++ src/main/java/com/example/apisistemanotas/ApiSistemaNotasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisistemanotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaNotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSistemaNotasApplication.class, args);
+    }
+
+}
