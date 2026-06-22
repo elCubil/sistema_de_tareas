8a5b8eb1f8a0e53673e05cfecc28681a0877a598
elCubil <rodriguez.arias.miguel@gmail.com>
2026-06-22 10:52:32 -0500

    solo endpoints /usuario no requieren credenciales

--- src/main/java/com/example/apisistemanotas/SecurityConfig.java
+++ src/main/java/com/example/apisistemanotas/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
@@ -12,7 +13,10 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf->csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/usuario").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
